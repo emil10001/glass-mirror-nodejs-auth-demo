96034b9b05456c5fb814e1138adad494a1e13909
E John Feig <ejohn@feigdev.com>
2014-02-16 13:24:39 -0800

    added location sending, no longer using config file

--- app.js
+++ app.js
@@ -3,114 +3,153 @@
  */
 
 var express = require('express')
-, routes = require('./routes')
-, http = require('http')
-, path = require('path')
-, googleapis = require('googleapis')
-, OAuth2Client = googleapis.OAuth2Client
-, config = require('./config.json');
+    , http = require('http')
+    , googleapis = require('googleapis')
+    , OAuth2Client = googleapis.OAuth2Client;
 
-var oauth2Client = new OAuth2Client(config.CLIENT_ID, config.CLIENT_SECRET, config.REDIRECT_URL);
+// Use environment variables to configure oauth client.
+// That way, you never need to ship these values, or worry
+// about accidentally committing them
+var oauth2Client = new OAuth2Client(process.env.MIRROR_DEMO_CLIENT_ID,
+    process.env.MIRROR_DEMO_CLIENT_SECRET, process.env.MIRROR_DEMO_REDIRECT_URL);
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 58305);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
+app.set('port', 8081);
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
     app.use(express.errorHandler());
 }
 
-var success = function(data) { console.log('success',data); };
-var failure = function(data) { console.log('failure',data); };
+var success = function (data) {
+    console.log('success', data);
+};
+var failure = function (data) {
+    console.log('failure', data);
+};
 var gotToken = function () {
     googleapis
-    .discover('mirror', 'v1')
-    .execute(function(err, client) {
-        if (!!err){
-            failure();
-            return;
-        }
-        console.log('mirror client',client);
-        listTimeline(client, failure, success);
-        insertHello(client, failure, success);
-        insertContact(client, failure, success);
-    });
+        .discover('mirror', 'v1')
+        .execute(function (err, client) {
+            if (!!err) {
+                failure();
+                return;
+            }
+            console.log('mirror client', client);
+            listTimeline(client, failure, success);
+            insertHello(client, failure, success);
+            insertContact(client, failure, success);
+            insertLocation(client, failure, success);
+        });
 };
 
 // send a simple 'hello world' timeline card with a delete option
-var insertHello = function(client, errorCallback, successCallback){
+var insertHello = function (client, errorCallback, successCallback) {
     client
-    .mirror.timeline.insert(
+        .mirror.timeline.insert(
         {
             "text": "Hello world",
-            "menuItems": [{"action": "DELETE"}]
+            "callbackUrl": "https://mirrornotifications.appspot.com/forward?url=http://localhost:8081/reply",
+            "menuItems": [
+                {"action": "REPLY"},
+                {"action": "DELETE"}
+            ]
         }
     )
-    .withAuthClient(oauth2Client)
-    .execute(function(err, data){
-        if (!!err)
-            errorCallback(err);
-        else
-            successCallback(data);
-    });
+        .withAuthClient(oauth2Client)
+        .execute(function (err, data) {
+            if (!!err)
+                errorCallback(err);
+            else
+                successCallback(data);
+        });
 };
-var insertContact = function(client, errorCallback, successCallback){
+
+// send a simple 'hello world' timeline card with a delete option
+var insertLocation = function (client, errorCallback, successCallback) {
     client
-    .mirror.contacts.insert(
+        .mirror.timeline.insert(
         {
-            "id": "harold",
-            "displayName": "Harold Penguin",
-            "iconUrl": "https://developers.google.com/glass/images/harold.jpg",
+            "text": "Let's meet at the Hacker Dojo!",
+            "callbackUrl": "https://mirrornotifications.appspot.com/forward?url=http://localhost:8081/reply",
+            "location": {
+                "kind": "mirror#location",
+                "latitude": 37.4028344,
+                "longitude": -122.0496017,
+                "displayName": "Hacker Dojo",
+                "address": "599 Fairchild Dr, Mountain View, CA"
+            },
+            "menuItems": [
+                {"action":"NAVIGATE"},
+                {"action": "REPLY"},
+                {"action": "DELETE"}
+            ]
+        }
+    )
+        .withAuthClient(oauth2Client)
+        .execute(function (err, data) {
+            if (!!err)
+                errorCallback(err);
+            else
+                successCallback(data);
+        });
+};
+
+
+var insertContact = function (client, errorCallback, successCallback) {
+    client
+        .mirror.contacts.insert(
+        {
+            "id": "emil10001",
+            "displayName": "emil10001",
+            "iconUrl": "https://secure.gravatar.com/avatar/bc6e3312f288a4d00ba25500a2c8f6d9.png",
             "priority": 7,
             "acceptCommands": [
+                {"type": "REPLY"},
                 {"type": "POST_AN_UPDATE"},
                 {"type": "TAKE_A_NOTE"}
             ]
         }
     )
-    .withAuthClient(oauth2Client)
-    .execute(function(err, data){
-        if (!!err)
-            errorCallback(err);
-        else
-            successCallback(data);
-    });
+        .withAuthClient(oauth2Client)
+        .execute(function (err, data) {
+            if (!!err)
+                errorCallback(err);
+            else
+                successCallback(data);
+        });
 };
-var listTimeline = function(client, errorCallback, successCallback){
+var listTimeline = function (client, errorCallback, successCallback) {
     client
-    .mirror.timeline.list()
-    .withAuthClient(oauth2Client)
-    .execute(function(err, data){
-        if (!!err)
-            errorCallback(err);
-        else
-            successCallback(data);
-    });
+        .mirror.timeline.list()
+        .withAuthClient(oauth2Client)
+        .execute(function (err, data) {
+            if (!!err)
+                errorCallback(err);
+            else
+                successCallback(data);
+        });
 };
-var grabToken = function (code, errorCallback, successCallback){
-    oauth2Client.getToken(code, function(err, tokens){
-        if (!!err){
+var grabToken = function (code, errorCallback, successCallback) {
+    oauth2Client.getToken(code, function (err, tokens) {
+        if (!!err) {
             errorCallback(err);
         } else {
-            console.log('tokens',tokens);
+            console.log('tokens', tokens);
             oauth2Client.credentials = tokens;
             successCallback();
         }
     });
 };
 
-app.get('/', function(req,res){
-    if (!oauth2Client.credentials){
+app.get('/', function (req, res) {
+    if (!oauth2Client.credentials) {
         // generates a url that allows offline access and asks permissions
         // for Mirror API scope.
         var url = oauth2Client.generateAuthUrl({
@@ -121,16 +160,25 @@ app.get('/', function(req,res){
     } else {
         gotToken();
     }
-    res.render('index', { title: 'Glass Mirror API with Node' });
+    res.write('Glass Mirror API with Node');
     res.end();
 
 });
-app.get('/oauth2callback', function(req, res){
+app.get('/oauth2callback', function (req, res) {
     // if we're able to grab the token, redirect the user back to the main page
-    grabToken(req.query.code, failure, function(){ res.redirect('/'); });
+    grabToken(req.query.code, failure, function () {
+        res.redirect('/');
+    });
+});
+app.post('/reply', function(req, res){
+    console.log('replied',req);
+    res.end();
+});
+app.post('/location', function(req, res){
+    console.log('location',req);
+    res.end();
 });
 
-
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
